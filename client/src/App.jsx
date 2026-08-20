9dfe17c4b56b468f669fab3ea0f17c0c7ec34213
Lubaib Mir <lubaibnaseer854@gmail.com>
2026-08-20 18:34:23 +0530

    fix: react-router-dom import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './components/layout/AppLayout'
 import Dashboard from './pages/Dashboard'
 import Inventory from './pages/Inventory'
